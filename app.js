bcc7578bf35be4362c1fc21ee9dcd837c3a49751
gungdeaditya <gunkdep@gmail.com>
2017-05-16 17:20:56 +0700

    initial commit and add sequlize

--- app.js
+++ app.js
@@ -21,25 +21,28 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // routes
 app.use('/api', index)
+app.use('/', (req, res, next) => {
+    res.status(200).send('Gungde')
+})
 app.use('/api/users', users)
 app.use('/api/books', books)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
+app.use(function(req, res, next) {
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
 })
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.send('error')
+    // render the error page
+    res.status(err.status || 500)
+    res.send('error')
 })
 
 module.exports = app
